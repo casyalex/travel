833bb25c3c1d4b37de773dda5b4daf25bb6c4552
AlexL <casyalex@hotmail.com>
2018-06-21 23:43:58 +0800

    推荐布局样式

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -16,7 +16,8 @@ export default {
       return {
           swiperOption: {
               pagination: '.swiper-pagination',
-              loop: true
+              loop: true,
+              autoplay: 1000
           },
           swiperList: [{
               id: '0001',
