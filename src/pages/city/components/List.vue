e951bb34319b291faec3f8d04b5c12c9953ad5ee
xyx <superalex_1104@163.com>
2018-06-25 16:43:01 +0800

    布局

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,190 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-topbottom">当前城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">热门城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        北京
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CityList"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.border-topbottom 
+    &:before 
+        border-color: #cccccc;
+    &:after 
+        border-color: #cccccc;
+.border-bottom 
+    &:before 
+        border-color: #cccccc;
+.list
+    position absolute
+    overflow hidden
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+        line-height: 0.4rem;
+        background: #eeeeee;
+        padding-left: 0.2rem;
+        color: #666;
+        font-size: 0.26rem;
+
+    .button-list
+        overflow: hidden;
+        padding: 0.1rem 0.6rem 0.1rem 0.1rem;
+        .button-wrapper 
+            float: left;
+            width: 33%;
+            .button 
+                padding: 0.1rem 0;
+                margin: 0.1rem;
+                text-align: center;
+                border: 0.02rem solid #ccc;
+                border-radius: 0.06rem;
+    .item-list
+        .item
+            line-height .76rem
+            color #666666
+            padding-left .2rem
+</style>
+
